a70abd453ce95949f81b11c421c43bb50443d45d
Fatec <faelpower3d>
2024-05-14 20:17:58 -0300

    falta itemproduto no pedido

--- inclusao/pedido.php
+++ inclusao/pedido.php
@@ -9,25 +9,15 @@
     //Verifica se o Formulario foi submetido
     if ($_SERVER["REQUEST_METHOD"] == "POST"){
         include ("../conexaoBanco/loja.php");
-        $nome = $_POST["nome"];
-        $endereco = $_POST["endereco"];
-        $numero = $_POST["numero"];        
-        $bairro = $_POST["bairro"];
-        $cidade = $_POST["cidade"];
-        $estado = $_POST["estado"];
-        $email = $_POST["email"];
-        $cpf_cnpj = $_POST["cpf_cnpj"];
-        $rg = $_POST["rg"];
-        $telefone = $_POST["telefone"];
-        $celular = $_POST["celular"];
-        $data_nasc = $_POST["data_nasc"];        
-        $salario = $_POST["salario"];
+        $data = $_POST["data"];
+        $id_cliente = $_POST["id_cliente"];
+        $observacao = $_POST["observacao"];        
+        $forma_pagto = $_POST["forma_pagto"];
+        $prazo_entrega = $_POST["prazo_entrega"];
+        $id_vendedor = $_POST["id_vendedor"];        
         
-        $query = "INSERT INTO  clientes(nome,endereco,numero,bairro,cidade,estado,email,
-        cpf_cnpj,rg,telefone,celular,data_nasc,salario)
-        VALUES ('$nome','$endereco','$numero',
-        '$bairro','$cidade','$estado','$email','$cpf_cnpj','$rg','$telefone','$celular',
-        '$data_nasc','$salario')";
+        $query = "INSERT INTO pedidos ('data', id_cliente, observacao, forma_pagto, prazo_entrega, id_vendedor) 
+        VALUES ('$data', '$id_cliente', '$observacao', '$forma_pagto', '$prazo_entrega', '$id_vendedor')";
         $result = mysqli_query($con, $query);
         if (mysqli_insert_id($con)) {
             echo "Inclusão realizada com sucesso ! !";
@@ -38,42 +28,65 @@
     }   
     ?>
     <form method="post">
-    <label> Nome: </label>
-        <input type="text" size="80" maxlength="100" name="nome" required>        
-        <br><label> Endereço: </label>
-        <input type="text" size="80" maxlength="100" name="endereco" required>
-        <label> Número: </label>
-        <input type="number" maxlength="10" name="numero" required>        
-        <label> Bairro: </label>
-        <input type="text" size="60" maxlength="60" name="bairro" required>
-        <label> Cidade: </label>
-        <input type="text" size="40" maxlength="80" name="cidade" required>
-        <label> Estado: </label>        
-        <select name="estado">
-            <option value="SP">SP</option>
-            <option value="MG">MG</option>
-            <option value="PR">PR</option>
-            <option value="RJ">RJ</option> </select> 
-        <label> e-mail: </label>
-        <input type="email" size="40" maxlength="80" name="email" required>  
-        <label> CPF/CNPJ :</label>
-        <input type="text" size="11" maxlength="11" name="cpf_cnpj" required>     
-        <label> RG :</label>
-        <input type="text" size="9" maxlength="9" name="rg" required>             
-        <label> Telefone: </label>
-        <input type="tel" maxlength="14" name="telefone" placeholder="(XX)XXXX-XXXX" required>
-        <label> Celular: </label>
-        <input type="tel" maxlength="14" name="celular" placeholder="(XX)XXXXX-XXXX" required>  
-        <label>Data de nascimento</label>      
-        <input type="date" name="data_nasc" required>
-        <label> Salario</label>
-        <input type="number" min="0" max="50000" name="salario" required>         
-        </select>
-        <p><input type="submit" value="Enviar"> <input type="reset" value="Limpar">
-    </form>
-    <a href="../consulta/cliente.php"><button>Voltar</button></a>     
-           
+    <label> data: </label>
+        <input type="date" size="80" maxlength="100" name="data" required> 
+        <label> cliente: </label> 
+        <select name="id_cliente">
+        <?php
+        include ("../conexaoBanco/loja.php");
+        $query = 'SELECT * FROM clientes ORDER BY nome;';
+        $resu = mysqli_query($con, $query) or die (mysqli_connect_error());
+        while ($reg = mysqli_fetch_array($resu)) {
+        ?>
+        
+         <option value="<?php echo $reg ['id'];?>"> <?php echo $reg ['nome'];?>
+        </option>         
+        <?php
+        }
+        mysqli_close($con);
+        ?>
+        </select>      
+        <br><label> Observação: </label>
+        <input type="text" size="80" maxlength="100" name="observacao" required>
+        <label>Forma de pagamento</label>
+        <select name="forma_pagto" >
+        <?php
+        include ("../conexaoBanco/loja.php");
+        $query = 'SELECT * FROM forma_pagto ORDER BY descricao;';
+        $resu = mysqli_query($con, $query) or die (mysqli_connect_error());
+        while ($reg = mysqli_fetch_array($resu)) {
+        ?>
+         <option value="<?php echo $reg ['id'];?>"> <?php echo $reg ['descricao'];?>
+        </option>         
+        <?php
+        }
+        mysqli_close($con);
+        ?>
+        </select>  
 
-    
+        
+
+
+        <label> prazo de prazo_entrega: </label>
+        <input type="date" maxlength="10" name="prazo_entrega" required>          
+        
+        </select>
+        <label> Vendedor (a): </label>       
+        <select name="vendedor">
+        <?php
+        include ("../conexaoBanco/loja.php");
+        $query = 'SELECT * FROM vendedor ORDER BY nome;';
+        $resu = mysqli_query($con, $query) or die (mysqli_connect_error());
+        while ($reg = mysqli_fetch_array($resu)) {
+        ?>
+         <option value="<?php echo $reg ['id'];?>"> <?php echo $reg ['nome'];?>
+        </option>         
+        <?php
+        }
+        mysqli_close($con);
+        ?>       
+        <br><input type="submit" value="Enviar"> <input type="reset" value="Limpar">
+    </form>
+    <a href="../index.html"><button>Voltar</button></a>  
 </body>
 </html>
