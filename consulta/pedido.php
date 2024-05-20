929a6f70cbd86a8652dbb7410473fa8c59c88b1d
Fatec <faelpower3d>
2024-05-20 19:54:42 -0300

    visualiza pedidos finalizado

--- consulta/pedido.php
+++ consulta/pedido.php
@@ -15,15 +15,16 @@
             $resu=mysqli_query($con, $query) or die (mysqli_connect_error()); 
             $query2 = "Select * FROM itens_pedido";
             $resu2=mysqli_query($con, $query2) or die (mysqli_connect_error());
-            $query3="Select observacao,prazo_entrega,id_vendedor FROM pedidos order by data"; 
+            $query3="Select id,forma_pagto,prazo_entrega,id_vendedor,observacao FROM pedidos order by data"; 
             $resu3=mysqli_query($con, $query3) or die (mysqli_connect_error());
             echo "<tr><td><b> DATA";
             echo "<td><b> CLIENTE</td>";
             echo "<td><b> PRODUTO</td>";
             echo "<td><b> QUANTIDADE</td>";
-            echo "<td><b> OBSERVAÇÃO</td>";
+            echo "<td><b> FORMA PAGAMENTO</td>";
             echo "<td><b> PRAZO/ENTREGA</td>";
-            echo "<td><b> VENDEDOR(A)</td>";            
+            echo "<td><b> VENDEDOR(A)</td>"; 
+            echo "<td><b> OBSERVAÇÃO</td>";           
             while ($reg1 = mysqli_fetch_array($resu)) { 
                 echo "<tr><td>".$reg1['data']. "</td>";                 
                 echo "<td>".$reg1['id_cliente']. "</td>";
@@ -33,12 +34,15 @@
                     echo "<td>".$reg['qtde']. "</td>";
                 }  
                 while ($reg = mysqli_fetch_array($resu3)) {                               
-                echo "<td>".$reg['observacao']. "</td>";                 
+                echo "<td>".$reg['forma_pagto']. "</td>";                 
                 echo "<td>".$reg['prazo_entrega']. "</td>";                 
-                echo "<td>".$reg['id_vendedor']. "</td>";        
+                echo "<td>".$reg['id_vendedor']. "</td>";
+                echo "<td>".$reg['observacao']. "</td>";  
+                echo "<td><a href='../edita/cliente.php?id=". $reg['id']."'>Detalhes</a></td>"; 
+                echo "<td><a href='../excluir/cliente.php?id=". $reg['id']. "'>Excluir </a></td></tr>";
+
             }   
-            echo "<td><a href='../edita/cliente.php?id=". $reg1['id']."'>Detalhes</a></td>"; 
-            echo "<td><a href='../excluir/cliente.php?id=". $reg1['id']. "'>Excluir </a></td></tr>";
+            
                   
             mysqli_close($con); 
         ?>        
