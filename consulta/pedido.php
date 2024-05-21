bf859a369f768f9d5c5da214bb325031d20ffd53
unknown <faelpower3d>
2024-05-21 14:59:47 -0300

    view criada e funcionando

--- consulta/pedido.php
+++ consulta/pedido.php
@@ -11,12 +11,8 @@
         <table border="1" width="100%"> 
         <?php 
             include ('../conexaoBanco/loja.php'); 
-            $query="Select data,id_cliente FROM pedidos order by data"; 
-            $resu=mysqli_query($con, $query) or die (mysqli_connect_error()); 
-            $query2 = "Select * FROM itens_pedido";
-            $resu2=mysqli_query($con, $query2) or die (mysqli_connect_error());
-            $query3="Select id,forma_pagto,prazo_entrega,id_vendedor,observacao FROM pedidos order by data"; 
-            $resu3=mysqli_query($con, $query3) or die (mysqli_connect_error());
+            $query="Select * FROM ver_pedidos order by data"; 
+            $resu=mysqli_query($con, $query) or die (mysqli_connect_error());            
             echo "<tr><td><b> DATA";
             echo "<td><b> CLIENTE</td>";
             echo "<td><b> PRODUTO</td>";
@@ -25,18 +21,14 @@
             echo "<td><b> PRAZO/ENTREGA</td>";
             echo "<td><b> VENDEDOR(A)</td>"; 
             echo "<td><b> OBSERVAÇÃO</td>";           
-            while ($reg1 = mysqli_fetch_array($resu)) { 
-                echo "<tr><td>".$reg1['data']. "</td>";                 
-                echo "<td>".$reg1['id_cliente']. "</td>";
-            } 
-                while($reg= mysqli_fetch_array($resu2))  {
-                    echo "<td>".$reg['id_produto']. "</td>";                 
-                    echo "<td>".$reg['qtde']. "</td>";
-                }  
-                while ($reg = mysqli_fetch_array($resu3)) {                               
-                echo "<td>".$reg['forma_pagto']. "</td>";                 
+            while ($reg = mysqli_fetch_array($resu)) { 
+                echo "<tr><td>".$reg['data']. "</td>";                 
+                echo "<td>".$reg['clientes']. "</td>";            
+                echo "<td>".$reg['produto']. "</td>";                 
+                echo "<td>".$reg['quantidade']. "</td>";
+                echo "<td>".$reg['forma_pagamento']. "</td>";                 
                 echo "<td>".$reg['prazo_entrega']. "</td>";                 
-                echo "<td>".$reg['id_vendedor']. "</td>";
+                echo "<td>".$reg['vendedor']. "</td>";
                 echo "<td>".$reg['observacao']. "</td>";  
                 echo "<td><a href='../edita/cliente.php?id=". $reg['id']."'>Detalhes</a></td>"; 
                 echo "<td><a href='../excluir/cliente.php?id=". $reg['id']. "'>Excluir </a></td></tr>";
